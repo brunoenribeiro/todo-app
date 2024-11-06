ddf536cc72867c5b3bf1814c8edbf8267cd9fc8a
Bruno Nunes <brunoenribeiro@gmail.com>
2024-11-06 09:20:42 -0300

    refactor(todos): move todo item to its own file

--- src/ChallengeComponent.tsx
+++ src/ChallengeComponent.tsx
@@ -1,45 +1,7 @@
 import { useState, useMemo } from "react";
 import type { Todo } from "./types";
 import AddTodoForm from "./components/AddTodoForm";
-
-function TodoItem({
-  data,
-  onBack,
-  onNext,
-}: {
-  data: Todo;
-  onBack: (todoId: Todo["id"]) => void;
-  onNext: (todoId: Todo["id"]) => void;
-}) {
-  const isFirstStatus = useMemo(() => data.status === 0, [data.status]);
-  const isLastStatus = useMemo(() => data.status === 2, [data.status]);
-
-  return (
-    <div
-      style={{
-        border: "1px solid currentColor",
-        width: "100%",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          gap: "1rem",
-          padding: "1rem",
-          justifyContent: "space-between",
-        }}
-      >
-        <button disabled={isFirstStatus} onClick={() => onBack(data.id)}>
-          &lt;-
-        </button>
-        <span>{data.text}</span>
-        <button disabled={isLastStatus} onClick={() => onNext(data.id)}>
-          -&gt;
-        </button>
-      </div>
-    </div>
-  );
-}
+import TodoItem from "./components/TodoItem";
 
 export function ChallengeComponent() {
   const [todos, setTodos] = useState<Todo[]>([]);
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,41 @@
+import { useMemo } from "react";
+import { Todo } from "../types";
+
+export default function TodoItem({
+  data,
+  onBack,
+  onNext,
+}: {
+  data: Todo;
+  onBack: (todoId: Todo["id"]) => void;
+  onNext: (todoId: Todo["id"]) => void;
+}) {
+  const isFirstStatus = useMemo(() => data.status === 0, [data.status]);
+  const isLastStatus = useMemo(() => data.status === 2, [data.status]);
+
+  return (
+    <div
+      style={{
+        border: "1px solid currentColor",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          padding: "1rem",
+          justifyContent: "space-between",
+        }}
+      >
+        <button disabled={isFirstStatus} onClick={() => onBack(data.id)}>
+          &lt;-
+        </button>
+        <span>{data.text}</span>
+        <button disabled={isLastStatus} onClick={() => onNext(data.id)}>
+          -&gt;
+        </button>
+      </div>
+    </div>
+  );
+}
