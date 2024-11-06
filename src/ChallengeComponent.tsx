848b29b7497e36b2293f2062ab3b0820bed07293
Bruno Nunes <brunoenribeiro@gmail.com>
2024-11-06 09:10:28 -0300

    feat(todos): implement todo creation, status change, rendering

--- src/ChallengeComponent.tsx
+++ src/ChallengeComponent.tsx
@@ -1,10 +1,179 @@
-import React from 'react'
+import { FormEvent, useState, useMemo } from "react";
+
+type Todo = {
+  id: string;
+  text: string;
+  status: 0 | 1 | 2;
+};
+
+function TodoItem({
+  data,
+  onBack,
+  onNext,
+}: {
+  data: Todo;
+  onBack: (todoId: Todo["id"]) => void;
+  onNext: (todoId: Todo["id"]) => void;
+}) {
+  const isFirstStatus = useMemo(() => data.status === 0, [data.status]);
+  const isLastStatus = useMemo(() => data.status === 2, [data.status]);
+
+  return (
+    <div
+      style={{
+        border: "1px solid currentColor",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          padding: "1rem",
+          justifyContent: "space-between",
+        }}
+      >
+        <button disabled={isFirstStatus} onClick={() => onBack(data.id)}>
+          &lt;-
+        </button>
+        <span>{data.text}</span>
+        <button disabled={isLastStatus} onClick={() => onNext(data.id)}>
+          -&gt;
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export function ChallengeComponent() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const pendingTodos = useMemo(
+    () => todos.filter((todo) => todo.status === 0),
+    [todos]
+  );
+  const inProgressTodos = useMemo(
+    () => todos.filter((todo) => todo.status === 1),
+    [todos]
+  );
+  const doneTodos = useMemo(
+    () => todos.filter((todo) => todo.status === 2),
+    [todos]
+  );
+
+  function handleAddTodo(e: FormEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const text = formData.get("text") as string;
+    setTodos([...todos, { id: Date.now().toString(), text, status: 0 }]);
+  }
+
+  function handleTodoBack(todoId: Todo["id"]) {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.id === todoId);
+    const todo = newTodos[todoIndex];
+    if (todo.status === 0) return;
+    todo.status -= 1;
+    setTodos(newTodos);
+  }
+
+  function handleTodoNext(todoId: Todo["id"]) {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.id === todoId);
+    const todo = newTodos[todoIndex];
+    if (todo.status === 2) return;
+    todo.status += 1;
+    setTodos(newTodos);
+  }
+
   return (
     <>
-      {/* Delete this h2, and add your own code here. */}
-      <h2 style={{ textAlign: 'center', paddingTop: 200, paddingBottom: 200 }}>Your code goes here</h2>
+      <section
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          gap: "1rem",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            border: "1px solid currentColor",
+            padding: "1rem",
+            gap: "0.5rem",
+          }}
+        >
+          <h2 style={{ textAlign: "center" }}>To Do</h2>
+          {pendingTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              data={todo}
+              onBack={handleTodoBack}
+              onNext={handleTodoNext}
+            />
+          ))}
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            border: "1px solid currentColor",
+            padding: "1rem",
+            gap: "0.5rem",
+          }}
+        >
+          <h2 style={{ textAlign: "center" }}>In Progress</h2>
+          {inProgressTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              data={todo}
+              onBack={handleTodoBack}
+              onNext={handleTodoNext}
+            />
+          ))}
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            border: "1px solid currentColor",
+            padding: "1rem",
+            gap: "0.5rem",
+          }}
+        >
+          <h2 style={{ textAlign: "center" }}>Done</h2>
+          {doneTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              data={todo}
+              onBack={handleTodoBack}
+              onNext={handleTodoNext}
+            />
+          ))}
+        </div>
+      </section>
+
+      <section style={{ marginTop: "2rem" }}>
+        <form
+          onSubmit={handleAddTodo}
+          style={{ display: "flex", gap: "0.5rem" }}
+        >
+          <input
+            type="text"
+            name="text"
+            placeholder="Add task"
+            required
+            style={{ padding: "0.5rem" }}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </section>
     </>
-  )
+  );
 }
